608042138d363ebb059dfc1d96f43573d06ee159
Odeh Daniels <odehdaniel1@gmail.com>
2021-01-24 23:58:03 +0100

    Card Component init

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Text, View, StyleSheet, TextInput, Button } from 'react-native'
+import Card from '../components/Card';
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
             <Text style={styles.title}> Start a New Game </Text>
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
                 <TextInput />
                 <View style={styles.buttonContainer}>
-                    <Button title="Reset" onPress={() => {}} />
-                    <Button title="Confirm" onPress={() => {}} />
+                    <View style={styles.button}>
+                        <Button title="Reset" onPress={() => { }} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="Confirm" onPress={() => {}} />
+                    </View>
                 </View>
-            </View>
+            </Card>
         </View>
     );
 
@@ -28,19 +33,10 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginVertical: 10
     },
-    //elevation - shows shadow @Andriod
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center',
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        backgroundColor: 'white',
-        elevation: 5,
-        padding: 20,
-        borderRadius: 10
+        alignItems: 'center'
     },
     buttonContainer: {
         flexDirection: 'row',
@@ -48,9 +44,9 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         paddingHorizontal: 15
     },
-    // button: {
-    //     borderRadius: 30
-    // }
+    button: {
+        width: 100
+    }
 });
 
 export default StartGameScreen
